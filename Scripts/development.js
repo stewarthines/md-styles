9a82ef068646dcb9600bb218d0e8e781130bad1a
Stewart Hines <stewartwadehines@gmail.com>
2013-06-26 23:37:07 -0500

    added sliding function to the side nav.

---
+++ Scripts/development.js
@@ -0,0 +1,21 @@
+﻿$('document').ready(function () {
+    var $asideNav = $('aside[role="navigation"]')
+    var $asideNavOpen = $('aside[role="navigation"].aside-open')
+    var $asideNavClosed = $('aside[role="navigation"].aside-closed')
+    var $menuButton = $('#menuButton')
+
+    //Add closed to make sure it's closed on page load.
+    $asideNav.addClass("aside-closed");
+
+    //When menuButton is clicked add open class to aside
+    $menuButton.click(function () {
+        if ($asideNav.hasClass('aside-closed')) {
+            $asideNav.removeClass("aside-closed");
+            $asideNav.addClass("aside-open");
+        }
+        else {
+            $asideNav.removeClass('aside-open');
+            $asideNav.addClass('aside-closed');
+        }
+    });
+});
